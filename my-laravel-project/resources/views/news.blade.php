e649c0f71283d4d81434909b079ecc7ba7371d42
Vu Chien <vuchien990525@gmail.com>
2023-06-21 14:56:24 +0900

    chien

--- my-laravel-project/resources/views/news.blade.php
+++ my-laravel-project/resources/views/news.blade.php
@@ -26,67 +26,11 @@ $today = Carbon::now()->format('Y/m/d');
                             <div class="newspage-people">NGUYEN</div>
                             <p class="newspage-link">お知らせテスト </p>
                         </div>
-                        <div role="listitem" class="collection-item w-dyn-item">
-                            <div class="newspage-time">2023/05/16</div><a href="/news/2023-05-16"
-                                class="link-block-3 w-inline-block">
-                                <div class="newspage-people">NGUYEN</div><a href="/news/2023-05-01"
-                                    class="link-block-3 w-inline-block">
-                                    <p class="newspage-link">お知らせテスト </p>
-                                </a>
-                        </div>
-                        <div role="listitem" class="collection-item w-dyn-item">
-                            <div class="newspage-time">2023/05/16</div><a href="/news/2023-05-16"
-                                class="link-block-3 w-inline-block">
-                                <div class="newspage-people">NGUYEN</div><a href="/news/2023-05-01"
-                                    class="link-block-3 w-inline-block">
-                                    <p class="newspage-link">お知らせテスト </p>
-                                </a>
-                        </div>
-                        <div role="listitem" class="collection-item w-dyn-item">
-                            <div class="newspage-time">2023/05/16</div><a href="/news/2023-05-16"
-                                class="link-block-3 w-inline-block">
-                                <div class="newspage-people">NGUYEN</div><a href="/news/2023-05-01"
-                                    class="link-block-3 w-inline-block">
-                                    <p class="newspage-link">お知らせテスト </p>
-                                </a>
-                        </div>
-                        <div role="listitem" class="collection-item w-dyn-item">
-                            <div class="newspage-time">2023/05/16</div><a href="/news/2023-05-16"
-                                class="link-block-3 w-inline-block">
-                                <div class="newspage-people">NGUYEN</div><a href="/news/2023-05-01"
-                                    class="link-block-3 w-inline-block">
-                                    <p class="newspage-link">お知らせテスト </p>
-                                </a>
-                        </div>
-                        <div role="listitem" class="collection-item w-dyn-item">
-                            <div class="newspage-time">2023/05/16</div><a href="/news/2023-05-16"
-                                class="link-block-3 w-inline-block">
-                                <div class="newspage-people">NGUYEN</div><a href="/news/2023-05-01"
-                                    class="link-block-3 w-inline-block">
-                                    <p class="newspage-link">お知らせテスト </p>
-                                </a>
-                        </div>
-                        <div role="listitem" class="collection-item w-dyn-item">
-                            <div class="newspage-time">2023/05/16</div>
-                            <div class="newspage-people">NGUYEN</div>
-                            <p class="newspage-link">お知らせテスト </p>
-                        </div>
-                        <div role="listitem" class="collection-item w-dyn-item">
-                            <div class="newspage-time">2023/05/16</div>
-                            <div class="newspage-people">NGUYEN</div>
-                            <p class="newspage-link">お知らせテスト </p>
-                        </div>
-                        <!-- test -->
                     </div>
                 </div>
             </div>
         </div>
-        <!-- <div class="container-13 w-container"><a href="/service/news" target="_blank"
-                class="btn-secondary-center outline w-inline-block">
-                <div class="btn-text">もっと見る</div>
-            </a></div> -->
     </div>
-
 </main>
 @endsection
 @section('scripts')
