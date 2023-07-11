ecc78470a037409f7ef9321a652b0559e07ed953
kai12111999 <btung19991112@gmail.com>
2023-07-11 11:02:40 +0900

    NGUYEN

--- my-laravel-project/resources/views/login.blade.php
+++ my-laravel-project/resources/views/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
     <link rel="stylesheet" href="{{asset('css\login.css')}}">
     <title>ログイン</title>
 </head>
+
 <body>
     <div class="container-fluid main">
         <div class="loginBox p-4">
@@ -16,7 +18,7 @@
                     <h1><strong>ログイン</strong></h1>
                 </div>
                 @if (session('message'))
-                    {{session('message')}}               
+                {{session('message')}}
                 @endif
                 <form action="/login" method="post">
                     @csrf
@@ -26,14 +28,14 @@
                             <img src="{{asset('img/t.webp')}}" alt="">
                         </div>
                         @if ($errors->has('tel'))
-                            <span class="error">{{ $errors->first('tel') }}</span>
+                        <span class="error">{{ $errors->first('tel') }}</span>
                         @endif
                         <div class="passArea">
                             <input type="password" name="password" id="" placeholder="パスワード">
                             <img src="{{asset('img/lock.png')}}" alt="">
                         </div>
                         @if ($errors->has('password'))
-                            <span class="error">{{ $errors->first('password') }}</span>
+                        <span class="error">{{ $errors->first('password') }}</span>
                         @endif
                     </div>
                     <div class="button"><input class="btn btn-primary btnlogin" type="submit" value="ログイン"></div>
@@ -42,4 +44,5 @@
         </div>
     </div>
 </body>
+
 </html>
--- my-laravel-project/resources/views/syukkin.blade.php
+++ my-laravel-project/resources/views/syukkin.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
@@ -8,62 +9,62 @@
     <title>勤退登録画面</title>
     <link rel="stylesheet" href="{{asset('css/syukkin.css')}}">
 </head>
+
 <body>
-     <div class="containt">
+    <div class="containt">
         <section class="leawork">
             <div class="leaworkArea">
-            <div class="currtimefiled">
-                <span class="currDate">2022年00月00日(月)</span>
-                <span class="currTime">00:00</span>
-            </div>
-            <div class="leawork">
+                <div class="currtimefiled">
+                    <span class="currDate">2022年00月00日(月)</span>
+                    <span class="currTime">00:00</span>
+                </div>
+                <div class="leawork">
                     <button id="start">
                         <img src="./img/t.png" alt="">
-                        <span>出勤</span> 
+                        <span>出勤</span>
                     </button>
                     <button id="end">
                         <img src="./img/63z7Bolx8cP5pkcxNhY2GA==.svg" alt="">
-                        <span>退勤</span> 
+                        <span>退勤</span>
                     </button>
+                </div>
             </div>
-       </div>
         </section>
         <section class="fileter">
-        <div class="leavetime">
-            <ul class="leavetimeArea">
-                @foreach ($syukkins as $syukkin)
-                <li>
-                    <span>{{$syukkin->staff_name}}さん</span>
-                    <span>{{$syukkin->time}}に{{$syukkin->type}}</span>
-                </li>
-                @endforeach
-            </ul>
-        </div>
+            <div class="leavetime">
+                <ul class="leavetimeArea">
+                    @foreach ($syukkins as $syukkin)
+                    <li>
+                        <span>{{$syukkin->staff_name}}さん</span>
+                        <span>{{$syukkin->time}}に{{$syukkin->type}}</span>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
             <div class="customer-number">
                 <h2>同伴者数</h2>
                 <ul class="customer-number-list" id='customer-list'>
-                <li id="customer-select-number"><span>0</span></li>
-                @for ($i = 1; $i <= 50; $i++)
-                    <li><span>{{$i}}</span></li>
-                @endfor
+                    <li id="customer-select-number"><span>0</span></li>
+                    @for ($i = 1; $i <= 50; $i++) <li><span>{{$i}}</span></li>
+                        @endfor
                 </ul>
             </div>
             <div class="emp-number">
                 <h2>担当人数</h2>
-            <ul class="emp-number-list" id='employee-list'>
-                <li id ="emp-select-number"><span>0</span></li>
-                @for ($i = 1; $i <= 50; $i++)
-                    <li><span>{{$i}}</span></li>
-                @endfor
+                <ul class="emp-number-list" id='employee-list'>
+                    <li id="emp-select-number"><span>0</span></li>
+                    @for ($i = 1; $i <= 50; $i++) <li><span>{{$i}}</span></li>
+                        @endfor
                 </ul>
             </div>
         </section>
-     </div>
-     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-     
-    </body>
-</html> 
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+</body>
+
+</html>
 <script>
-    
+
 </script>
-<script src= "{{asset('js/time.js')}}"></script>
+<script src="{{asset('js/time.js')}}"></script>
