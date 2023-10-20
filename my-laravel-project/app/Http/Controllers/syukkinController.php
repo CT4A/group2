1e1a4755e391635b16ec52d2a03f99b7234f743b
Vu Chien <vuchien990525@gmail.com>
2023-10-20 14:13:02 +0900

    chien

--- my-laravel-project/app/Http/Controllers/syukkinController.php
+++ my-laravel-project/app/Http/Controllers/syukkinController.php
@@ -6,6 +6,7 @@ use App\Models\attend_leave;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class syukkinController extends Controller
 {
